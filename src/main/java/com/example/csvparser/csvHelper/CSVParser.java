ab37f1ef59532b0976da1489a8a76b5f6c7a1813
omickelsen <omickelsen@gmail.com>
2019-10-28 22:43:59 -0600

    cleaning up code and testing with postman

--- src/main/java/com/example/csvparser/csvHelper/CSVParser.java
+++ src/main/java/com/example/csvparser/csvHelper/CSVParser.java
@@ -4,7 +4,7 @@ public class CSVParser {
     public String parseCSVString(String csv) {
 
 
-        String finishedOutput = "yeeeeeeet";
+        String finishedOutput = "";
 
 
         String[] lines;
@@ -88,89 +88,3 @@ public class CSVParser {
 
 }
 
-
-//
-//    let lines = [];
-//        lines = csvString.split(/\r\n|\n/); // splits at \r\n or \n and assigns the input to array lines
-//
-//
-//
-//        for ( let i = 0; i < lines.length; i++) {
-//        let line;
-//        line = lines[i]
-//
-//        finishedString = "";
-//        let pieces = [];
-//        pieces = line.split(""); // splits each character
-//
-//        //takes lines at index i and sets it to line. then it takes line and splits it at each character and assigns it to array pieces
-//        let startsWithQuote = false;
-//        // assigns startsWithQuote to boolean false in preparation for the if statement
-//        let tempX = "";
-//
-//        if (pieces[0] == '"'){
-//        startsWithQuote = true;
-//        }
-//
-//
-//        let parcedStr = "";
-//        startNewPiece = false;
-
-//        for (let i = 1; i < pieces.length; i++) {
-//        if (startsWithQuote) {
-//        if (startNewPiece == true) {
-//
-//        if (pieces[i] == '"') {
-//        startNewPiece = false;
-//        }
-//        } else if (pieces[i] != '"'){
-//        tempX += pieces [i];
-//        } else {
-//        parcedStr += "[" + tempX + "]";
-//
-//        finishedString += parcedStr;
-//        parcedStr = "";
-//        tempX = "";
-//        startNewPiece = true;
-//
-//        if(finishedString.length == 0) {
-//        }
-//
-//        if ( i + 2 < pieces.length && pieces [i + 2] != '"'){
-//        startsWithQuote = false;
-//        }
-//        }
-//
-//        } else {
-//        if (pieces [i] == ',' && tempX.length == 0){
-//        }
-//        else if (pieces[i] != ','){
-//        tempX += pieces[i];
-//        }
-//        else{
-//        parcedStr += "[" + tempX + "]";
-//        finishedString += parcedStr;
-//        if(finishedString.length == 0) {
-//
-//        }
-//        parcedStr = "";
-//        tempX = "";
-//        startNewPiece = true;
-//        if (i + 1 < pieces.length && pieces [i + 1] == '"'){
-//        startsWithQuote = true;
-//        }
-//        }
-//        }
-//
-//        }
-//
-//        if (finishedOutput.length > 0){
-//        finishedOutput += "\n";
-//        }
-//        finishedOutput += finishedString;
-//
-//        }
-//
-//        return finishedOutput;
-//
-//        }
